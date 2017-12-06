84d48df839c76ab154f65ff7951c8c96cb5ba033
Aditya Kothari <adityakothari99@gmail.com>
2017-12-07 02:26:59 +0530

    Completed code for inverting matrices
    Using cached data

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates an object to cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    #sol is the inverse of the matrix
+    sol <- NULL
+    set <- function(y){
+      x <<- y
+      sol <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solvematrix) sol <<- solvematrix
+    getinverse <- function() sol
+  
+    list(set = set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function to compute the inverse of the matrix as returned by the function above
 
 cacheSolve <- function(x, ...) {
+       
         ## Return a matrix that is the inverse of 'x'
+        sol <- x$getinverse()
+        if(!is.null(sol)) {
+                 message("getting cached data")
+                 return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setinverse(sol)
+        return(sol)
 }
